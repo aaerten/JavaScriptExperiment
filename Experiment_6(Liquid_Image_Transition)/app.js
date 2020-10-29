0edceb2a479fc957a5dc60993eefc6c689ca06a7
ALP ERTEN <alperten95@gmail.com>
2020-10-29 17:59:16 +0300

    Sixth Commit

---
+++ Experiment_6(Liquid_Image_Transition)/app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity:0.2,
+    image1: "./image/image1.jpg",
+    image2: "./image/image2.jpg",
+    displacementImage:"./image/effect1.png"
+});
+
+//Use https://github.com/robin-dela/hover-effect for more info
